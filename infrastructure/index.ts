55b475d185f1e8d1df0d15d675fc95b5576a0a57
Dharti Patel <dharti@Dhartis-MacBook-Air.local>
2026-02-05 11:54:56 -0500

    infra: secure OpenWeather API key and deploy to Canada Central

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -93,7 +93,7 @@ const containerGroup = new containerinstance.ContainerGroup(
           {
             name: 'WEATHER_API_KEY',
             value: config.requireSecret('weatherApiKey'),
-},
+          },
         ],
         resources: {
           requests: {
